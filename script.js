1f96d879a59ecf5502af035ee0deb2ed8eb29741
yadhupulimoottil <yadhukrishnapkpulimoottil.@gmail.com>
2024-04-18 17:44:12 +0530

    home page and product scroll with js

--- script.js
+++ script.js
@@ -34,3 +34,12 @@ let n = 0;
     }
     changeSlide();
  })
+
+
+ const scrollContainer =document.querySelectorAll('.products');
+for (const item  of scrollContainer) {
+    item.addEventListener('wheel',(evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    });
+}
